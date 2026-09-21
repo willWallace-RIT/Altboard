3ce3f0821addc053d20c240f99311086c1d592c7
northbot <54906390+willWallace-RIT@users.noreply.github.com>
2026-09-21 19:18:56 -0400

    Create launcher_action.sh

---
+++ usr/local/bin/launcher_action.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Place your payload here
+echo "Steam overlay keyboard toggle triggered!" >> /tmp/steam_keyboard_trigger.log
+
+# Example command to run:
+# notify-send "Steam Keyboard" "Triggered on alternate close event!"
